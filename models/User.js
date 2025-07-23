641225be67af693e32100503c11e6e3efcd6ea0b
Renanpacheco <prenan86@gmail.com>
2025-07-23 20:18:16 -0300

    add register user

--- models/User.js
+++ models/User.js
@@ -4,15 +4,17 @@ const db = require("../db/conn");
 const User = db.define("User", {
   name: {
     type: DataTypes.STRING,
-    require: true,
+    allowNull: false,
   },
   email: {
     type: DataTypes.STRING,
-    require: true,
+    allowNull: false,
+    unique: true, // importante para evitar duplicações
   },
   password: {
     type: DataTypes.STRING,
-    require: true,
+    allowNull: false,
   },
 });
+
 module.exports = User;
